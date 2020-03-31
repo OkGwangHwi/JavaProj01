53540d4c61cbc235eee80f6a0cf3d43e660da364
Kosmo_14 <Kosmo_14@DESKTOP-BNGN1OG>
2020-03-31 19:27:22 +0900

    첫번째 프로젝트 화요일 커밋

--- src/project2/ver04/HighCreditAccount.java
+++ src/project2/ver04/HighCreditAccount.java
@@ -8,51 +8,5 @@ public class HighCreditAccount extends Account{
 		this.gradeAdd = gradeAdd;
 	}
 
-	@Override
-	public String getAccountNumber() {
-		return super.getAccountNumber();
-	}
 	
-	@Override
-	public void setAccountNumber(String accountNumber) {
-		super.setAccountNumber(accountNumber);
-	}
-	
-	@Override
-	public String getName() {
-		return super.getName();
-	}
-	
-	@Override
-	public void setName(String name) {
-		super.setName(name);
-	}
-	
-	@Override
-	public int getBalance() {
-		return super.getBalance();
-	}
-	
-	@Override
-	public void setBalance(int balance) {
-		super.setBalance(balance);
-	}
-	
-	@Override
-	public int getAdd() {
-		return super.getAdd();
-	}
-	
-	@Override
-	public void setAdd(int add) {
-		super.setAdd(add);
-	}
-	
-	public int getGradeAdd() {
-		return gradeAdd;
-	}
-	
-	public void setGradeAdd(int gradeAdd) {
-		this.gradeAdd = gradeAdd;
-	}
 }
